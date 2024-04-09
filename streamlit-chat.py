141eae51a91c01f81179f2219891e164e311aa44
Antonela <tommantonela@gmail.com>
2024-04-09 15:11:12 -0300

    Remove secret

--- streamlit-chat.py
+++ streamlit-chat.py
@@ -13,9 +13,8 @@ def create_copilot():
     print('---------------------------- create_copilot')
     chroma_db = "./patterns_chromadb"
     kstore = KnowledgeStore(path=chroma_db)
-    openai_api_key = st.secrets["OPENAI_API_KEY"]
     
-    #llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.0, openai_api_key=openai_api_key)    
+    #llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.0, openai_api_key=st.secrets["OPENAI_API_KEY"])    
     llm = HuggingFaceEndpoint(repo_id='mistralai/Mistral-7B-Instruct-v0.2', temperature=0.01, token=st.secrets['HUGGINGFACEHUB_API_TOKEN'])
     
     copilotqa = PatternsQACopilot(store=kstore, llm=llm)
